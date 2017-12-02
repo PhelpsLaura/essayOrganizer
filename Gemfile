019608663304094e1ccd0b17090618926a2905c7
Laura Phelps <laphelps64@gmail.com>
2017-12-02 11:33:54 -0500

    Basic landing page!

--- Gemfile
+++ Gemfile
@@ -5,7 +5,14 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+#Use jquery
+gem 'jquery-rails'
 
+#Use Materialize Sass
+gem 'materialize-sass'
+
+#Use redcarpet for markdown parsing
+gem 'redcarpet'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
--- app/controllers/essays_controller.rb
+++ app/controllers/essays_controller.rb
@@ -1,2 +1,48 @@
 class EssaysController < ApplicationController
+
+  def index
+    @essays = Essay.all
+  end
+
+  def show
+    @essay = Essay.find(params[:id])
+  end
+
+  def new
+    @essay = Essay.new
+  end
+
+  def create
+    @essay = Essay.new(essay_params)
+    if @essay.save
+      redirect_to essay_path(@essay)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @essay = Essay.find(params[:id])
+  end
+
+  def update
+    @essay = Essay.find(params[:id])
+    if @essay.update(essay_params)
+      redirect_to essay_path(@essay)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @essay = Essay.find(params[:id])
+    @essay.destroy
+
+    redirect_to essays_path
+  end
+
+  private
+    def essay_params
+      params.require(:essay).permit(:title, :due_date)
+    end
 end
