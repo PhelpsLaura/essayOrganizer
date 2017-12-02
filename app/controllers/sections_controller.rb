5d1aa022c0843fd94973202af2e955160465d15d
Laura Phelps <laphelps64@gmail.com>
2017-12-02 16:54:14 -0500

    sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,48 @@
 class SectionsController < ApplicationController
+  def show
+    @essay = Essay.find(params[:essay_id])
+    @section = @essay.sections.find(params[:id])
+  end
+
+  def new
+    @essay = Essay.find(params[:essay_id])
+    @section = @essay.sections.new
+  end
+
+  def create
+    @essay = Essay.find(params[:essay_id])
+    @section = @essay.sections.new(section_params)
+    if @section.save
+      redirect_to essay_path(@essay)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @essay = Essay.find(params[:essay_id])
+    @section = @essay.sections.find(params[:id])
+  end
+
+  def update
+    @essay = Essay.find(params[:essay_id])
+    @section = @essay.sections.find(params[:id])
+    if @section.update(section_params)
+      redirect_to essay_path(@essay)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @essay = Essay.find(params[:id])
+    @section = @essay.sections.find(params[:id])
+
+    redirect_to essay_path(@essay)
+  end
+
+  private
+    def section_params
+      params.require(:section).permit(:content, :notes)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :essays do
     resources :drafts
+    resources :sections
     resources :sources do
       resources :research_cards
     end
